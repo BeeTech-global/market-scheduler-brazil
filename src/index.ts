13cd301d0a46b4aff992a4640176161f71023d0c
Victor Faria <voffbm@gmail.com>
2021-12-21 18:07:18 -0300

    chore: lint fix

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import BrazilianHolidays from "./BrazilianHolidays";
-import MarketScheduler from "./MarketScheduler";
+import BrazilianHolidays from './BrazilianHolidays';
+import MarketScheduler from './MarketScheduler';
 
 const brazilianHolidays = new BrazilianHolidays(2020, 2099);
 
 const holidays = brazilianHolidays.getAll();
 
 const marketScheduler = new MarketScheduler(
-  "12:00",
-  "20:00",
-  "YYYY-MM-DD",
-  "YYYY-MM-DD HH:mm:ss",
+  '12:00',
+  '20:00',
+  'YYYY-MM-DD',
+  'YYYY-MM-DD HH:mm:ss',
   holidays
 );
 
